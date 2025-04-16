65dbb80d76c0a1122002d6ac2df1ca852a05ecc2
Uyu <uyutanaka@gmail.com>
2025-04-16 10:21:09 +0900

    Add initial project setup with metadata and description

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Uyu TANAKA
+Date: 2023-10-01
+Version: 1.0.0
+License: MIT License
+Description: Testing VSCode and Git.
+Conda Environment: <your-conda-environment-name>
+"""
